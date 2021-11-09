592925694b062b23a0b821fea2c9b4fe9ab31e3c
Leonardo Alonso <leonardoalonsososa@gmail.com>
2021-11-08 18:45:34 -0600

    ➕[CHAT] Add chat component to store and list chats by user

--- network/routes.js
+++ network/routes.js
@@ -1,10 +1,12 @@
 const message = require('../components/messages/network')
 const user = require('../components/user/netwrok')
+const chat = require('../components/chat/netwrok')
 
 const routes = function(server) {
 
     server.use('/message', message)
     server.use('/user', user)
+    server.use('/chat', chat)
 
 }
 
